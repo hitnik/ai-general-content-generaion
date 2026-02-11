51bd8a84e57aa9d50f019b36ede9cc2ae2d9cb4d
Aliaksandr Nikitsin <hitnik@gmail.com>
2026-02-11 21:52:04 +0100

    tts server

--- server/app.py
+++ server/app.py
@@ -1,20 +1,34 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, Form
+from fastapi.responses import FileResponse
 import whisper
 import tempfile
+from TTS.api import TTS
 
 app = FastAPI()
 model = whisper.load_model("large")  # or "base", "small", etc.
+MODEL_NAME = "tts_models/en/ljspeech/tacotron2-DDC"
+tts = TTS(model_name=MODEL_NAME, progress_bar=False)
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
 
 @app.post("/v1/stt")
-async def transcribe(file: UploadFile = File(...)):
+async def speech_to_text(file: UploadFile = File(...)):
     with tempfile.NamedTemporaryFile(delete=False) as tmp:
         tmp.write(await file.read())
         tmp_path = tmp.name
     result = model.transcribe(tmp_path)
     return {"text": result["text"]}
 
+
+@app.post("/v1/tts")
+async def text_to_spech(text: str = Form(...), speaker: str = Form(None)):
+    with tempfile.NamedTemporaryFile(suffix=".wav", delete=False) as tmpfile:
+        wav_path = tmpfile.name
+        if speaker and tts.speakers:
+            tts.tts_to_file(text=text, speaker=speaker, file_path=wav_path)
+        else:
+            tts.tts_to_file(text=text, file_path=wav_path)
+
+        # Return the WAV file
+        response = FileResponse(wav_path, media_type="audio/wav", filename="speech.wav")
+        # Optionally, schedule file cleanup after sending
+        return response
